89f45618c9aa505991cd02d6e9ed743385792099
chenping <867606984@qq.com>
2018-11-23 01:14:32 +0800

    2018-11-23..1:14

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,204 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <keep-alive>
+      <router-view v-if="$route.meta.keepAlive" />
+    </keep-alive>
+    <router-view v-if="!$route.meta.keepAlive" />
   </div>
 </template>
 
 <style lang="less">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  outline: none;
+  text-decoration: none;
+  list-style: none;
+}
+
+body,
+html {
+  height: 100%;
+}
+
+.vhcenter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.vhbetween {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.vbetween {
+  display: flex;
+  justify-content: space-between;
+}
+
+.vcenter {
+  display: flex;
+  align-items: center;
+}
+
+.v-container {
+  padding: 20px;
+  background: #fff;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  position: fixed;
+  top: 0;
+  left: e("calc(50% - 250px)");
+  width: 500px;
+  height: 100%;
+  padding-bottom: 150px;
+  overflow-y: scroll;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  border-right: 1px solid #ccc;
+  border-left: 1px solid #ccc;
+  background: #e4e4e4;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+
+.receive {
+  padding: 10px;
+
+  .container {
+    overflow: hidden;
+
+    .header_pic {
+      float: left;
+      margin-right: 5px;
+      display: block;
+      width: 56px;
+      height: 56px;
     }
+
+    .chat_content {
+      float: left;
+      padding: 10px;
+      width: 60%;
+      border-radius: 5px;
+      background: #2db7f5;
+    }
+
+    .text {
+      word-break: break-all;
+    }
+
+    .content_img {
+      img {
+        display: block;
+        text-align: left;
+        width: 90%;
+      }
+    }
+
+    .mixins {
+      p {
+        word-break: break-all;
+      }
+
+      img {
+        display: block;
+        // margin: 0 auto;
+        text-align: left;
+        width: 90%;
+      }
+    }
+  }
+}
+
+.send {
+  padding: 10px;
+
+  .container {
+    overflow: hidden;
+
+    .header_pic {
+      float: right;
+      margin-left: 5px;
+      display: block;
+      width: 56px;
+      height: 56px;
+    }
+
+    .chat_content {
+      float: right;
+      padding: 10px;
+      width: 60%;
+      border-radius: 5px;
+      background: #2db7f5;
+    }
+
+    .text {
+      word-break: break-all;
+    }
+
+    .content_img {
+      img {
+        display: block;
+        text-align: right;
+        width: 90%;
+      }
+    }
+
+    .mixins {
+      p {
+        word-break: break-all;
+      }
+
+      img {
+        display: block;
+        // margin: 0 auto;
+        text-align: right;
+        width: 90%;
+      }
+    }
+  }
+}
+
+.bottom {
+  position: fixed;
+  bottom: 0;
+  left: e("calc(50% - 250px)");
+  width: 500px;
+  height: 130px;
+  border: 3px solid #e4e4e4;
+  background: #fff;
+
+  .func {
+    padding: 0 30px;
+    height: 30px;
+    border: 2px solid #e4e4e4;
+
+    ul {
+      height: 100%;
+
+      li {
+        cursor: pointer;
+        margin-right: 30px;
+
+        img {
+          display: block;
+          width: 20px;
+          height: 20px;
+        }
+      }
+    }
+  }
+
+  textarea {
+    display: block;
+    margin: 5px auto;
+    width: 90%;
+    height: 90px;
+    resize: none;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,100 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    
+    <!-- 接收 -->
+    <div class="receive">
+      <div class="container">
+        <img class="header_pic" src="../assets/headPic.png" alt="">
+        <div class="chat_content">
+          <div v-if="false" class="text">
+            <p>
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+            </p>
+          </div>
+
+          <div v-if="false" class="content_img">
+            <img src="../assets/1.jpg" alt="">
+          </div>
+
+          <div class="mixins">
+            <p>
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+            </p>
+            <img src="../assets/1.jpg" alt="">
+          </div>
+        </div>
+      </div>
+
+      
+    </div>
+
+    <!-- 发送 -->
+    <div class="send">
+      <div class="container">
+        <img class="header_pic" src="../assets/headPic.png" alt="">
+        <div class="chat_content">
+          <div v-if="false" class="text">
+            <p>
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+            </p>
+          </div>
+
+          <div v-if="false" class="content_img">
+            <img src="../assets/1.jpg" alt="">
+          </div>
+
+          <div class="mixins">
+            <p>
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+              weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+            </p>
+            <img src="../assets/1.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--输入框 -->
+    <div class="bottom">
+      <div class="func">
+        <ul class="vcenter">
+          <li>
+            <img class="updata" src="../assets/picUpdata.png" alt="">
+          </li>
+          <li>
+            <img class="comment" src="../assets/pingjia.png" alt="">
+          </li>
+        </ul>
+      </div>
+      <textarea></textarea>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components: {}
 })
 export default class Home extends Vue {}
 </script>
